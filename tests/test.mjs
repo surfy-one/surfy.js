3ff3c58e2d94ac42783ea4a355ad4430b0b10515
Alexander Yermolenko <i@surfy.one>
2023-04-01 11:38:29 +0500

    Added Translate Test

--- tests/test.mjs
+++ tests/test.mjs
@@ -11,3 +11,24 @@ import Surfy from '../src/surfy.mjs';
 import conf from '../conf.json' assert { type: 'json' };
 const surfy = new Surfy(conf);
 console.log(surfy)
+
+let options, result;
+
+/*
+
+Check Translate
+
+*/
+
+options = {
+	source: {
+		'lang': 'en',
+		'text': 'Hello Surfy!'
+	},
+	target: {
+		'lang': 'fr'
+	}
+};
+
+result = await surfy.words.translate(options);
+console.log(result);
