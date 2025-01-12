602ed03ea7c0d446eae5e546f270a6a00d492ef9
Alexander Yermolenko <i@surfy.one>
2025-01-12 06:30:06 +0000

    Sync

--- src/words.mjs
+++ src/words.mjs
@@ -24,7 +24,7 @@ class Words {
 
 		*/
 		
-		this.translate.languages = async () => {
+		this.translate.language = async () => {
 			
 			const result = await request({
 				url: `${this.conf.endpoint}/words/translate/languages`,
@@ -34,6 +34,38 @@ class Words {
 
 			return result;
 		}
+	}
+
+	/*
+
+	Get Language Information
+	
+	@Success Response JSON:
+	{
+		"name": "French",      // The name of the detected language in English
+		"native": "Français",  // The name of the detected language in its native form
+		"code": "fr",          // ISO 639-1 language code
+		"status": true         // Success
+	}
+
+	@Failure Response JSON:
+	{
+		"status": false        // Failed
+	}
+	
+	*/
+
+	async lang(text) {
+		const result = await request({
+			url: `${this.conf.endpoint}/words/lang`,
+			method: 'POST',
+			headers: this.conf.headers,
+			params: {
+				text: text
+			}
+		});
+
+		return result;
 	}
 
 	async translate(params) {
